71a22afbf44aa5fdbcb853e28ad4463c64b443ac
Shraddharai412 <shraddharai412@gmail.com>
2025-06-08 00:49:07 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import react from 'react';
+
+import Birthday from './Components/BirthdayCounter';
+function App()
+{
+  return(
+    <div>
+      <Birthday/>
+      
+    </div>
+   
+  );
+
+}
+export default App;
